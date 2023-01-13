cf1deef10fee7494ec34a8d1c341967a9158916c
Paweł Pepek <pepek@riskout.pl>
2023-01-13 23:11:14 +0100

    CoursesStats styling

--- src/components/navbar/CoursesStats.tsx
+++ src/components/navbar/CoursesStats.tsx
@@ -8,12 +8,12 @@ function CoursesStats() {
 
 	return (
 		<div className={`w-100 h-100 d-flex justify-content-around pb-2`}>
-			<div className='d-flex flex-column justify-content-center'>
+			<div className='d-flex flex-column justify-content-center align-items-center'>
 				<CertificateIcon
 					classesAdd={classes["icon-tabler-certificate"]}></CertificateIcon>
 				<p className={`h3 m-0 p-0 mt-auto ${classes.number}`}>{coursesCount}</p>
 			</div>
-			<div className='d-flex flex-column justify-content-center'>
+			<div className='d-flex flex-column justify-content-center align-items-center'>
 				<ClockIcon classesAdd={classes["icon-tabler-clock"]}></ClockIcon>
 				<p className={`h3 m-0 p-0 mt-auto ${classes.number}`}>{hours}h</p>
 			</div>
