bf95c1b3a41b2ba2223423f5d2667c13c2c8024a
Paweł Pepek <pepek@riskout.pl>
2023-01-13 23:39:26 +0100

    CertificatBadge as a button

--- src/components/author-card/body/badges/CertificateBadge.tsx
+++ src/components/author-card/body/badges/CertificateBadge.tsx
@@ -1,6 +1,7 @@
 import useCertificateShow from "../../../../hooks/use-certificate-show"
 import { Certificate } from "../../../../models/interfaces"
 import CertificateIcon from "../../../shared/icons/CertificateIcon"
+import classes from "./CertificateBadge.module.css"
 
 function CertificateBadge(props: Certificate) {
 	const dispatch = useCertificateShow()
@@ -10,12 +11,12 @@ function CertificateBadge(props: Certificate) {
 	}
 
 	return (
-		<a
+		<button
 			data-tip='Show certificate'
 			onClick={certificateClick}
-			className='d-flex flex-row-reverse text-primary'>
+			className={`d-flex flex-row-reverse text-primary border border-0 m-0 p-0 ${classes["certificate-button"]} d-print-none`}>
 			<CertificateIcon classesAdd='pointer-icon'></CertificateIcon>
-		</a>
+		</button>
 	)
 }
 
